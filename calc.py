01092dcf15782d333ae5b50d3b7a222f7a1d063c
CodePlayer <147758009+nzaxsdcfgvbhnj@users.noreply.github.com>
2024-10-22 09:19:36 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,81 +1,65 @@
-#!/usr/bin/env python
-
-# Copyright 2016 -- Levi Starrett & Jay Hankins
-# for educational purposes only
-#
-# For use in CS 190: https://github.com/Purdue-CSUSB/CS-190-F2016/
-#
-# Calculator -- a four function calculator commandline tool
-
-import sys
+#!/usr/bin/env python3
 
 # -------------------------------------------------------- #
 # -- CALCULATOR FUNCTIONS -------------------------------- #
 # -------------------------------------------------------- #
 
 # Add function
-# a -- addend
-# b -- augend
 def add(a, b):
     return a + b
 
 # Subtract function
-# a -- minuend
-# b -- subtrahend
 def sub(a, b):
     return a - b
 
 # Multiply function
-# a -- multiplicand
-# b -- multiplier
 def mult(a, b):
     return a * b
 
 # Divide function
-# a -- dividend
-# b -- divisor
 def div(a, b):
+    if b == 0:
+        return "Error: Division by zero is undefined."
     return a / b
 
 
 # -------------------------------------------------------- #
 
-
 # -------------------------------------------------------- #
-# -- MAIN FUNCTIONAILTY -- DO NOT EDIT ------------------- #
+# -- MAIN FUNCTIONALITY ---------------------------------- #
 # -------------------------------------------------------- #
 
 a = None
 b = None
 op = None
 
-while (True):
+while True:
     # get input values
-    a = raw_input("Enter the first argument: ")
-    op = raw_input("Enter the operation: ")
-    b = raw_input("Enter the second argument: ")
+    a = input("Enter the first argument: ")
+    op = input("Enter the operation (+, -, *, /): ")
+    b = input("Enter the second argument: ")
     try:
         a = int(a)
         b = int(b)
     except ValueError:
-        print "Invalid number argument..."
+        print("Invalid number argument...")
         op = None
 
     # decide function
-    if (op != None):
-        if (op == "+"):
-            print "Sum: ", add(a, b)
-        elif (op == "-"):
-            print "Difference: ", sub(a, b)
-        elif (op == "*"):
-            print "Product: ", mult(a, b)
-        elif (op == "/"):
-            print "Quotient: ", div(a, b)
+    if op is not None:
+        if op == "+":
+            print("Sum: ", add(a, b))
+        elif op == "-":
+            print("Difference: ", sub(a, b))
+        elif op == "*":
+            print("Product: ", mult(a, b))
+        elif op == "/":
+            print("Quotient: ", div(a, b))
         else:
-            print "Invalid operation..."
+            print("Invalid operation...")
 
-    q = raw_input("Quit? [y/n] ")
-    if (q == "y" or q == "Y"):
+    q = input("Quit? [y/n] ")
+    if q.lower() == "y":
         break
 
 # -------------------------------------------------------- #
